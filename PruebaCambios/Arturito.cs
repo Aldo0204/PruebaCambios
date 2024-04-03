b037c5467561f6dae351168753518cb37679e586
Aldo0204 <a2213720211@alumnos.uat.edu.mx>
2024-04-02 19:20:34 -0500

    Cuando pasa pasa

---
+++ PruebaCambios/Arturito.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCambios
+{
+    internal class Arturito
+    {
+        //Puto el que lo lea
+    }
+}
